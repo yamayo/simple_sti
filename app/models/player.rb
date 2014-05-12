adabb3cca0ced26affff50e75e67244e5ea79e18
yamayo <yamatoyo@gmail.com>
2014-05-12 19:29:12 +0900

    some changes

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,11 @@
 class Player < ActiveRecord::Base
+  class << self
+    def find_sti_class(type_name)
+      type_name.classify.constantize
+    end
+
+    def sti_name
+      name.downcase
+    end
+  end
 end
